030125fe30b09014ac37e8a051e7c51d6cf34405
dhruv354 <dhruvsin@iitk.ac.in>
2021-05-22 20:37:42 +0530

    added template for my blog and static files from start bootstrap

--- server.py
+++ server.py
@@ -1,8 +1,24 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
+@app.route("/index.html")
+def home():
     return render_template('index.html')
+
+#about page
+@app.route('/about.html')
+def about():
+    return render_template('about.html')
+
+#post page
+@app.route('/post.html')
+def post():
+    return render_template('post.html')
+
+#contact.html
+@app.route('/contact.html')
+def contact():
+    return render_template('contact.html')
+
 
 app.run(debug=True)
