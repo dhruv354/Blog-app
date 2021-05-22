c28df6a3d4a9cd37535b10fc308ab36f79bc1e4f
dhruv354 <dhruvsin@iitk.ac.in>
2021-05-22 20:58:14 +0530

    added recurring layout in layout.html and using template inheritance to avoid copied code

--- server.py
+++ server.py
@@ -1,22 +1,22 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/index.html")
+@app.route("/")
 def home():
     return render_template('index.html')
 
 #about page
-@app.route('/about.html')
+@app.route('/about')
 def about():
     return render_template('about.html')
 
 #post page
-@app.route('/post.html')
+@app.route('/post')
 def post():
     return render_template('post.html')
 
 #contact.html
-@app.route('/contact.html')
+@app.route('/contact')
 def contact():
     return render_template('contact.html')
 
